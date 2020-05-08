8821de8619f6a0b2f0296050066b11439c840791
Kathlyn Olson <kolson25@my.stlcc.edu>
2020-05-07 23:14:28 -0500

    album models added

---
+++ Models/Album.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations; //Namespace is used to add propery level attributes
+using System.Diagnostics.CodeAnalysis;
+
+namespace ArtistCollection.Models
+{
+    public class Album
+    {
+        [Required(ErrorMessage = "Album Id is required.")]
+        public int AlbumID { get; set; }
+
+        //[Required(ErrorMessage ="Album Name is required.")]
+
+        [Required(ErrorMessage = "Album Name is required.")]
+        public string AlbumName { get; set; }
+
+        //[Required(ErrorMessage ="Year is required.")]
+        [AllowNull]
+        public int Year { get; set; }
+
+        //[Required(ErrorMessage="Genre is required.")]
+        [AllowNull]
+        public string Genre { get; set; }
+
+
+
+
+    }
+}
