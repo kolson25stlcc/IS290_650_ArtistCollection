94b1cc5a84ccf2c49266162068829e4a2a9f16dc
Kathlyn Olson <kolson25@my.stlcc.edu>
2020-05-13 10:13:02 -0500

    Back to delete link

--- Models/Artist.cs
+++ Models/Artist.cs
@@ -10,25 +10,34 @@ namespace ArtistCollection.Models
 {
     public class Artist
     {
+        [Display(Name = "Artist ID")]
         [Required(ErrorMessage = "Artist Id is required.")]
         public int ArtistID { get; set; }
 
+        [Display(Name = "First Name")]
         [Required(ErrorMessage = "Artist's first name is required.")]
         public string ArtistFirstName { get; set; }
 
+        [Display(Name = "Last Name")]
         [Required(ErrorMessage = "Artist's last name is required.")]
         public string ArtistLastName { get; set; }
 
+        [Display(Name = "The yeear the artist was born")]
         [Required(ErrorMessage = "The year the artist was born is required.")]
         public int YearBorn { get; set; }
 
+        [Display(Name = "The year the artist Died")]
+        [Required(ErrorMessage = "The year the artist died is required. ")]
         public int? YearDied { get; set; }
 
+        [Display(Name = "Number of years active")]
+        [Required(ErrorMessage = "The number of years the artist has been active is required. ")]
         public int? YearsActive { get; set; }
 
         [AllowNull]
         public string Occupation { get; set; }
 
+        [Column(TypeName = "decimal(10,2)")]
         public decimal? NetWorth { get; set; }
 
         [AllowNull]
@@ -46,7 +55,7 @@ namespace ArtistCollection.Models
         public string Photo { get; set; }
 
 
-        public ICollection<Artist_Song_Collection> Artist_Song_Collections { get; set; }
+        // public ICollection<Artist_Song_Collection> Artist_Song_Collections { get; set; }
 
 
     }
